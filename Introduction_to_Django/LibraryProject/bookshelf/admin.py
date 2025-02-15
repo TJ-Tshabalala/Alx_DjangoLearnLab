ba086a6688f05445155602797db3dc07c0db4687
fakeLemonz <tshabalalatity14@gmail.com>
2025-02-15 23:45:45 +0200

    Correct search_field config

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,7 +4,7 @@ from .models import Book
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     list_filter = "title","author", "publication_year"
-    search_field = "title","author", "publication_year"
+    search_fields = "title","author", "publication_year"
     list_display = "title","author", "publication_year"
 
 admin.site.register(Book,BookAdmin)
