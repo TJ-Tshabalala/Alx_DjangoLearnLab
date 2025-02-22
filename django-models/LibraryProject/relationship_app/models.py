56d6bc64e2435ab2267e666763343299bef00e83
fakeLemonz <tshabalalatity14@gmail.com>
2025-02-22 23:42:18 +0200

    Modify relationship_app models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,15 +3,21 @@ from django.db.models import Model
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length = 100)
+    
+    return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length = 100)
     author = models.ForeignKey(Author, on_delete = models.CASCADE)
     
+    return self.title
 class Library(models.Model):
     name = models.CharField(max_length = 100)
     books = models.ManyToManyField(Book)
+    return self.name
 
 class Librarian(models.Model):
     name = models.CharField(max_length = 100)
     library = models.OneToOneField(Library,on_delete = models.CASCADE)
+
+    return self.name
