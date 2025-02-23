09dc0d1775e333524d777cd8f9b67cfde6681efb
fakeLemonz <tshabalalatity14@gmail.com>
2025-02-24 00:31:35 +0200

    Totally

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,6 +4,19 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
 class Author(models.Model):
     name = models.CharField(max_length=100)
   
