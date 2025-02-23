8e7239892c8fe70b15509ee445a7d86138df69c3
fakeLemonz <tshabalalatity14@gmail.com>
2025-02-24 00:35:34 +0200

    Totally

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('librarian/',views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
      path('books/add/', views.add_book, name='add_book'),
-    path('books/change/<int:pk>/', views.change_book, name='change_book'),
+    path('books/nge/<int:pk>/', views.change_book, name='change_book'),
+    path('books/edit/<int:pk>/', views.change_book, name='edit_book'),
     path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
         ]
