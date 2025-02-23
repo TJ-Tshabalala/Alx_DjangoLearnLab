ea2edd68399c2fb841ae438e94a5408f25b6e0c2
fakeLemonz <tshabalalatity14@gmail.com>
2025-02-23 23:50:50 +0200

    Totally cooked

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,11 +3,12 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', views.list_books, name='book_list'),
     path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/',views.login_view, name='login'),
-    path('logout/',views.logout_view, name='logout'),
+    path('login/',LoginView.as_view(template_name='relationship_app/login.html')),
+    path('logout/',LogoutView.as_view(template_name='relationship_app/logout.html')),
     path('register/', views.register_view, name='register'),
         ]
