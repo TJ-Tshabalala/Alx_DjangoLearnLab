19b5d229201b6dc7ed4661073b835616c234291d
fakeLemonz <tshabalalatity14@gmail.com>
2025-03-09 01:13:11 +0200

    Build First API Endpoint

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+from django.http import HttpReponse
+from django.urls import path
 
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+]
 # Create your views here.
