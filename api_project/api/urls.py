56aca1975cad80cfc76de210047fb9c16abba3d0
fakeLemonz <tshabalalatity14@gmail.com>
2025-03-09 09:27:44 +0200

    Update api/urls

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,12 +3,11 @@ from.views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
 
+router = DefaultRouter()
+router.register(r'books',BookViewSet, basename='book_all')
 urlpatterns =[
     # Route for the BookList View (ListAPIView)
     path('books/', BookList.as_view(), name='book_list')
-
-    router.register(r'books', BookViewSet, basename='book_all')
-
     # Include the router URLs for BookViewSet (all CRUD operations)
     path('',include(router.urls)), # This includes all routes to registered with the router
 ]
