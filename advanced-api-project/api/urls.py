daf6ce29c8f2ad2059aaad85e201eb288c28a588
fakeLemonz <tshabalalatity14@gmail.com>
2025-03-16 10:55:14 +0200

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,8 +3,8 @@ from .import views
 
 urlpatterns = [
     path('', views.BookListView.as_view(), name='book-list'),
-    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
-    path('book/create/', views.BookCreateView.as_view(), name='book-create'),
-    path('book/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),
-    path('book/<int:pk?/delete/', views.BookDeleteView.as_view(), name='book-delete'),
+    path('books/', views.BookDetailView.as_view(), name='book-detail'),
+    path('books/create', views.BookCreateView.as_view(), name='book-create'),
+    path('books/update', views.BookUpdateView.as_view(), name='book-update'),
+    path('books/delete', views.BookDeleteView.as_view(), name='book-delete'),
 ]
