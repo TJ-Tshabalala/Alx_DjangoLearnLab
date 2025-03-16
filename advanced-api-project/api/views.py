a95b37b18cb6e6e5145f4d6f286ba99ccd1766cd
fakeLemonz <tshabalalatity14@gmail.com>
2025-03-16 17:28:04 +0200

    Update views.py with testcases

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,46 +1,71 @@
-# api/views.py
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework import filters
-from django_filters import rest_framework 
-from .models import Book
-from .serializers import BookSerializer
-
-class BookListCreateView(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['title', 'author', 'publication_year'] # Allow filtering by year
-    search_fields = ['title', 'author__name']  # Search title and author name
-    ordering_fields = ['title', 'publication_year'] # Allow ordering by title and publication date
-
-class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
-# api/serializers.py
-from rest_framework import serializers
+# api/test_views.py
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
 from .models import Book, Author
+from .serializers import BookSerializer
+from django.urls import reverse
 
-class AuthorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = '__all__'
+class BookAPITestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.author = Author.objects.create(name='Test Author')
+        self.book = Book.objects.create(title='Test Book', author=self.author, publication_date='2023-01-01')
+        self.client.force_authenticate(user=self.user)
 
-class BookSerializer(serializers.ModelSerializer):
-    author = AuthorSerializer(read_only=True)
-    author_id = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all(), source='author', write_only=True)
+    def test_book_list_create(self):
+        url = reverse('book-list-create')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
 
-    class Meta:
-        model = Book
-        fields = '__all__'
+        data = {'title': 'New Book', 'author_id': self.author.id, 'publication_date': '2024-01-01'}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 2)
 
-    def create(self, validated_data):
-        validated_data.pop('author', None)
-        return super().create(validated_data)
+    def test_book_retrieve_update_destroy(self):
+        url = reverse('book-retrieve-update-destroy', kwargs={'pk': self.book.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Test Book')
 
-    def update(self, instance, validated_data):
-        validated_data.pop('author', None)
-        return super().update(instance, validated_data)
+        data = {'title': 'Updated Book', 'author_id': self.author.id, 'publication_date': '2024-01-01'}
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.get(pk=self.book.pk).title, 'Updated Book')
+
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
+
+    def test_book_filtering(self):
+        url = reverse('book-list-create') + '?title=Test%20Book'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], 'Test Book')
+
+    def test_book_search(self):
+        url = reverse('book-list-create') + '?search=Test%20Author'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['author']['name'], 'Test Author')
+
+    def test_book_ordering(self):
+        Book.objects.create(title='Another Book', author=self.author, publication_date='2022-01-01')
+        url = reverse('book-list-create') + '?ordering=publication_date'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]['title'], 'Another Book')
+
+    def test_unauthenticated_read_only(self):
+        self.client.force_authenticate(user=None)
+        url = reverse('book-list-create')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = {'title': 'New Book', 'author_id': self.author.id, 'publication_date': '2024-01-01'}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
