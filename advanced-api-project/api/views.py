9def5ed720df652ab955b9b01ee4ffafddd329dc
fakeLemonz <tshabalalatity14@gmail.com>
2025-03-16 17:22:42 +0200

    Update settings.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import filters
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework 
 from .models import Book
 from .serializers import BookSerializer
 
