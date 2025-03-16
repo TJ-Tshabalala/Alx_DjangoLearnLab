07766ccc90ccea39a6db92719f9189fcff4916de
fakeLemonz <tshabalalatity14@gmail.com>
2025-03-16 17:08:32 +0200

    Update permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse_lazy
 from .models import Book
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 @method_decorator(login_required, name='dispatch')
 class BookListView(ListView):
