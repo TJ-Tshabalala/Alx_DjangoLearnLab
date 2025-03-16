e0da04096d5958e533d0f47a552df574c1c3a7c6
fakeLemonz <tshabalalatity14@gmail.com>
2025-03-16 17:21:02 +0200

    Update settings.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,53 +1,46 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from .models import Book
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-
-@method_decorator(login_required, name='dispatch')
-class BookListView(ListView):
-    model = Book
-    template_name = 'book_list.html'
-    context_object_name = 'books'
-
-@method_decorator(login_required, name='dispatch')
-class BookDetailView(DetailView):
-   model = Book
-   template_name = 'book_detail.html'
-   context_object_name = 'book'
-
-@method_decorator(login_required, name='dispatch')
-class BookCreateView(CreateView):
-    model = Book
-    fields = ['title', 'author', 'publication_date']
-    template_name = 'book_form.html'
-
-@method_decorator(login_required, name='dispatch')
-class BookUpdateView(UpdateView):
-    model = Book
-    fields = ['title','author','publication_date']
-    template_name = 'book_form.html'
-
-@method_decorator(login_required, name='dispatch')
-class BookDeleteView(DeleteView):
-    model = Book
-    fields = ['title','author','publication_date']
-    success_url = reverse_lazy('book-list')
-# Create your views here.
 # api/views.py
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly] # Allow read for all, write for authenticated
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['title', 'author', 'publication_date__year'] # Allow filtering by year
+    search_fields = ['title', 'author__name']  # Search title and author name
+    ordering_fields = ['title', 'publication_date'] # Allow ordering by title and publication date
 
 class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly] # Allow read for all, write for authenticated
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+# api/serializers.py
+from rest_framework import serializers
+from .models import Book, Author
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+    author_id = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all(), source='author', write_only=True)
+
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def create(self, validated_data):
+        validated_data.pop('author', None)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data.pop('author', None)
+        return super().update(instance, validated_data)
