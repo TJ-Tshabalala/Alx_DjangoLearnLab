d9ae423f5b9f428e10cc3e32f3798ca18c180977
fakeLemonz <tshabalalatity14@gmail.com>
2025-03-22 15:58:57 +0200

    Add commenting system

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DeleteView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+from django.http import HttpResponseForbidden
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
@@ -10,6 +11,49 @@ from django.contrib.auth.models import User
 from .forms import CustomUserCreationForm
 from django.contrib import messages
 
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    comments = post.comments.all().order_by('-created_at')
+    form = CommentForm
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'forms': form})
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save()(commit = False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', post_id=post_id)
+        else:
+            form = CommentForm()
+            return render(request, 'blog/add_comment.html', {'form': form, 'post':post})
+        
+@login_required
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if comment.author != request.user:
+        return HttpResponseForbidden("You do not have permission to edit this comment.")
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', post_id=comment.post.id)
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'blog/edit_comment.html', {'form': form, 'comment': comment})
+
+@login_required
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if comment.author != request.user:
+        return HttpResponseForbidden("You do not have permission to delete this comment.")
+    post_id = comment.post.id
+    comment.delete()
+    return redirect('post_detail', post_id=post_id)
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
