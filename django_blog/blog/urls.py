7b2f51be47ef1e4fec0919888dd33ae1d20b9034
fakeLemonz <tshabalalatity14@gmail.com>
2025-03-22 16:10:10 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,9 +14,9 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('posts/<int:post_id>/comments/new/', views.add_comment, name='add_comment'),
-    path('comments/<int:comment_id>/edit/', views.edit_comment, name='edit_comment'),
-    path('comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
+    path('posts/<int:pk>/comments/new/', views.add_comment, name='add_comment'),
+    path('comments/<int:pk>/update/', views.edit_comment, name='edit_comment'),
+    path('comments/<int:pk>/delete/', views.delete_comment, name='delete_comment'),
 ]
 
 ]
